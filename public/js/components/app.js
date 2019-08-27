c173502e720e7149ae774bb3e17a8be9e530e078
Michael Sean De La Riva <michaeldlr17@gmail.com>
2019-08-27 09:58:03 -0700

    added JS mobile menu button to devices 767px and smaller. note: still width issues with samsung internet and safari...

---
+++ public/js/components/app.js
@@ -0,0 +1,27 @@
+var menuBtn = document.getElementsByClassName('menuButton')
+
+var mobileMenu = document.getElementsByClassName('mobileMenu')
+
+var clickedBtn = function() {
+    mobileMenu[0].classList.toggle('active')
+}
+
+menuBtn[0].addEventListener('click', clickedBtn)
+
+console.log(menuBtn[0])
+
+
+
+// close button ==============================================
+
+var closeBtn = document.getElementsByClassName('closeButton')
+
+var mobileMenu = document.getElementsByClassName('mobileMenu')
+
+var clickedBtn = function() {
+    mobileMenu[0].classList.toggle('active')
+}
+
+closeBtn[0].addEventListener('click', clickedBtn)
+
+console.log(menuBtn[0])
