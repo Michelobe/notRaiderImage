2c261a7b8433fe707ae970b1299b755f1d86c976
Michael Sean De La Riva <michaeldlr17@gmail.com>
2019-08-27 10:36:42 -0700

    attempting to fix overflow issue with samasung internet and safari.  change max-width for hovermenu to keep menu from going off screen.

--- public/js/components/app.js
+++ public/js/components/app.js
@@ -25,3 +25,18 @@ var clickedBtn = function() {
 closeBtn[0].addEventListener('click', clickedBtn)
 
 console.log(menuBtn[0])
+
+
+// // hoverMenu ==============================================
+
+// var closeBtn = document.getElementsByClassName('closeButton')
+
+// var mobileMenu = document.getElementsByClassName('mobileMenu')
+
+// var clickedBtn = function() {
+//     mobileMenu[0].classList.toggle('active')
+// }
+
+// closeBtn[0].addEventListener('click', clickedBtn)
+
+// console.log(menuBtn[0])
